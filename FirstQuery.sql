55a52d481a740ac3c242b4b3db0190f476b884fe
Brendon Cooper <coopsbre@hotmail.com>
2019-07-06 05:19:04 +1000

    Testing 123

---
+++ FirstQuery.sql
@@ -0,0 +1,15 @@
+select 4+9 As MyAnswer;
+GO
+select 15-26 As Balance;
+GO 
+select 24*4 As MyResponse;
+GO
+select 48/4 As Result;
+GO 
+select 'Hello World' As ColumnName
+
+ -- dbo:- Database Owner
+-- create table tblSecond (mynumbers int);
+
+ select * from INFORMATION_SCHEMA.COLUMNS
+ 
