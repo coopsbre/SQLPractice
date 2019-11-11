30548c8d779557d19ba83fef44f71c5e9a94d620
Brendon Cooper <coopsbre@hotmail.com>
2019-11-11 20:22:42 +1100

    Adjustments to the Work Order Details

--- WorkOderCreator/WorkOrderCreator/BusinessObjects/BO_WorkOrderDetail.cs
+++ WorkOderCreator/WorkOrderCreator/BusinessObjects/BO_WorkOrderDetail.cs
@@ -68,5 +68,64 @@ namespace WorkOrderCreator.BusinessObjects
 
             return DVR;
         }
+
+        public DataValidatorReturn Create(int workOrderID, int itemNumber, int activityID)
+        {
+
+            DVR = MethodHelper.IsGreaterThanZero("Work Order Number", workOrderID);
+
+            if (DVR.IsValid == false)
+                return DVR;
+
+            DVR = MethodHelper.IsGreaterThanZero("Item Number", itemNumber);
+
+            if (DVR.IsValid == false)
+                return DVR;
+
+            DVR = MethodHelper.IsGreaterThanZero("Activity ID", activityID);
+
+            if (DVR.IsValid == false)
+                return DVR;
+
+            if (Find(workOrderID,itemNumber,activityID).ItemFound)
+            {
+                DVR.IsValid = false;
+                DVR.ReturnText = "Work Order Detail # " + workOrderID.ToString() + " already exists.";
+                DVR.ItemFound = true;
+                return DVR;
+            }
+
+            using (var context = new WorkOrderLogEntities())
+            {
+                WorkOrderDetail workOrderDetail = new WorkOrderDetail()
+                {
+                    WOHdrID = workOrderID,
+                    ItemNumber = itemNumber,
+                    ActivityID = activityID
+                };
+
+                try
+                {
+                    context.WorkOrderDetails.Add(workOrderDetail);
+                    //Once we have created a work order detail need to look for the  
+                    //Activity using the activity Id, and then get the activityTypeId
+                    //Go to the sourcescripthdr table and get the sourcescripthdrid 
+                    //and use the info from the sourcescriptdtl table to create 
+                    //WorkOrderDtlScript records also need to create these actual files 
+                    //by retrieving the source items and pasting to the new files.
+                    context.SaveChanges();
+                    DVR.IsValid = true;
+                    DVR.ReturnText = "Work Order Detail #" + workOrderID.ToString() + " Added to the Database.";
+                    DVR.ReturnType = workOrderDetail;
+                }
+                catch (Exception exception)
+                {
+                    DVR.IsValid = false;
+                    DVR.ReturnText = exception.Message;
+                }
+            }
+
+            return DVR;
+        }
     }
 }
